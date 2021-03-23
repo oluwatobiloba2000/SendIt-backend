1df21dfb77571cb710667b2dd11a307c01224cf5
unknown <ananioluwatobiloba2000@gmail.com>
2021-03-23 02:41:53 +0100

    modified the email url

--- src/repository/user.js
+++ src/repository/user.js
@@ -298,12 +298,12 @@ export async function sendRecoveryAccountToken({ email }) {
   
                           <p class="description center" style="font-family:'Muli','Arial Narrow',Arial;Margin:0;text-align:center;max-width:320px;color:#a1a8ad;line-height:24px;font-size:15px;Margin-bottom:10px;margin-left: auto; margin-right: auto;"><span style="color: rgb(161, 168, 173); font-family: Muli, &quot;Arial Narrow&quot;, Arial; font-size: 15px; text-align: center; background-color: rgb(255, 255, 255);">That's okay, it happens! Click on the button below to reset your password.</span></p>
                           <!--[if (gte mso 9)|(IE)]><br>&nbsp;<![endif]--><span class="sg-image" data-imagelibrary="%7B%22width%22%3A%22260%22%2C%22height%22%3A54%2C%22alt_text%22%3A%22Reset%20your%20Password%22%2C%22alignment%22%3A%22%22%2C%22border%22%3A0%2C%22src%22%3A%22https%3A//marketing-image-production.s3.amazonaws.com/uploads/c1e9ad698cfb27be42ce2421c7d56cb405ef63eaa78c1db77cd79e02742dd1f35a277fc3e0dcad676976e72f02942b7c1709d933a77eacb048c92be49b0ec6f3.png%22%2C%22link%22%3A%22%23%22%2C%22classes%22%3A%7B%22sg-image%22%3A1%7D%7D">
-                          <a href="http://localhost:3000/password/reset?token=${token}&email=${email}" target="_blank">
+                          <a href="https://sendit.netlify.app/password/reset?token=${token}&email=${email}" target="_blank">
   <img alt="Reset your Password" height="54" src="https://marketing-image-production.s3.amazonaws.com/uploads/c1e9ad698cfb27be42ce2421c7d56cb405ef63eaa78c1db77cd79e02742dd1f35a277fc3e0dcad676976e72f02942b7c1709d933a77eacb048c92be49b0ec6f3.png" style="border-width: 0px; margin-top: 30px; margin-bottom: 50px; width: 260px; height: 54px;" width="260"></a></span>
                           <div>
                             <h3>Or Copy and paste this link in your browser</h3>
                             <div style="width: 500px; overflow: auto">
-                            http://localhost:3000/password/reset?token=${token}&email=${email}
+                            https://sendit.netlify.app/password/reset?token=${token}&email=${email}
                             </div>
                           </div>
                           <!--[if (gte mso 9)|(IE)]><br>&nbsp;<![endif]--></center>
